26f24786e87cf1642e99b9c4563083b5a9773bd0
Rayra2302 <91684591+Rayra2302@users.noreply.github.com>
2022-06-22 10:16:54 -0300

    Add files via upload

--- component/read.jsx
+++ component/read.jsx
@@ -3,7 +3,7 @@ import { useState,useEffect} from 'react'
 
 //importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc } from 'firebase/firestore';
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc } from 'firebase/firestore';
 
 //configurar o Firebase do projeto
 const contato = collection(database,'contato')
@@ -24,6 +24,26 @@ export default function Read() {
     read()
   },[])
 
+  const [ID,setID] = useState(null)
+  const [contatoUnico,setContatoUnico] = useState({})
+  const [mostrar,setMostrar] = useState(false)
+  const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [telefone, setTelefone] = useState("")
+  const [mensagem, setMensagem] = useState("")
+
+  const show = async(id)=>{
+
+    setID(id)
+    if(ID!=null){
+      const contatoSimples = doc(database,"contato", ID)
+      const resultado = await getDoc(contatoSimples)
+      setContatoUnico({...resultado.data(),id:resultado.id})
+      setMostrar(true)
+    }
+  }
+
+
 const deletebtn = (id)=>{
   const documento = doc(database,"contato",id)
   deleteDoc(documento)
@@ -34,6 +54,20 @@ const deletebtn = (id)=>{
 
   return (
     <>
+      {mostrar ?(
+        <div>
+          <h3 className="text-center">ALTERAR</h3>
+          <input type="text" name="nome" placeholder='Nome' className='form-control' id="" required onChange={event=>setNome(event.target.value)} value={nome} />
+        <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={email} />
+        <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={telefone} />
+        <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={mensagem} ></textarea>
+        <input type="submit" value="SALVAR" className='form-control btn btn-outline-dark' />
+        </div>
+      ):(
+        <></>
+      )}
+
+
        <h3 className='text-center'>GRAVADOS</h3>
         {contatoLista.map((lista)=>{
           return(
@@ -46,7 +80,7 @@ const deletebtn = (id)=>{
               </div>
               <div className='card-footer text-center'>
               <div className="input-group">
-              <input type="button" className='btn-outline-warning form-control' value="Alterar" />
+              <input type="button" onClick={()=>show(lista.id)} className='btn-outline-warning form-control' value="Alterar" />
               <input type="button"onClick={()=>deletebtn(lista.id)} className='btn-outline-danger form-control' value="Excluir" />  
               </div>
               </div>
