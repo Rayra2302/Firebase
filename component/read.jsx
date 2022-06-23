1c7f676a96517ba19935e8be37df67a9f2aea56e
Rayra2302 <91684591+Rayra2302@users.noreply.github.com>
2022-06-23 11:11:21 -0300

    Add files via upload

--- component/read.jsx
+++ component/read.jsx
@@ -3,7 +3,8 @@ import { useState,useEffect} from 'react'
 
 //importar a config do firebase
 import { app, database } from '../services/firebase'
-import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc } from 'firebase/firestore';
+import { collection,addDoc, getDocs, orderBy, query, doc, deleteDoc, getDoc, updateDoc } from 'firebase/firestore';
+
 
 //configurar o Firebase do projeto
 const contato = collection(database,'contato')
@@ -24,6 +25,14 @@ export default function Read() {
     read()
   },[])
 
+  const deletebtn = (id)=>{
+    const documento = doc(database,"contato",id)
+    deleteDoc(documento)
+    .then(()=>{
+      read()
+    })
+  }
+
   const [ID,setID] = useState(null)
   const [contatoUnico,setContatoUnico] = useState({})
   const [mostrar,setMostrar] = useState(false)
@@ -39,18 +48,46 @@ export default function Read() {
       const contatoSimples = doc(database,"contato", ID)
       const resultado = await getDoc(contatoSimples)
       setContatoUnico({...resultado.data(),id:resultado.id})
+      setNome(contatoUnico.nome)
+      setEmail(contatoUnico.email)
+      setTelefone(contatoUnico.telefone)
+      setMensagem(contatoUnico.mensagem) 
+    }
+    if(mensagem!=""){
       setMostrar(true)
     }
   }
+  useEffect(()=>{
+    show()
+  },[ID])
 
+  const bt_cancelar =()=>{
+    setMostrar(false)
+    setNome("")
+    setEmail("")
+    setTelefone("")
+    setMensagem("")
+    setID(null)
+  }
 
-const deletebtn = (id)=>{
-  const documento = doc(database,"contato",id)
-  deleteDoc(documento)
-  .then(()=>{
-    read()
-  })
-}
+  const bt_alterar =(id)=>{
+    const contatoShow = doc(database,'contato',id)
+    updateDoc(contatoShow,{
+      nome:nome,
+      email:email,
+      telefone:telefone,
+      mensagem:mensagem
+
+    }).then(()=>{
+      setNome("")
+      setEmail("")
+      setTelefone("")
+      setMensagem("")
+      setID(null)
+      read()
+      setMostrar(false)
+    })
+  }
 
   return (
     <>
@@ -61,7 +98,8 @@ const deletebtn = (id)=>{
         <input type="email" name="email" placeholder='Email' className='form-control' id="" required onChange={event=>setEmail(event.target.value)} value={email} />
         <input type="tel" name="telefone" placeholder='Telefone' className='form-control' id="" required onChange={event=>setTelefone(event.target.value)} value={telefone} />
         <textarea name="mensagem" className='form-control' placeholder='Mensagem' id="" onChange={event=>setMensagem(event.target.value)} value={mensagem} ></textarea>
-        <input type="submit" value="SALVAR" className='form-control btn btn-outline-dark' />
+        <input type="button" value="CANCELAR" className="form-control btn btn-outline-danger" onClick={bt_cancelar} />
+        <input type="submit" value="SALVAR" className='form-control btn btn-outline-dark' onClick={()=>bt_alterar(contatoUnico.id)} />
         </div>
       ):(
         <></>
